6b6050bd00bf0430bd55df9075eae3052cddfcdd
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-12 11:22:40 +0530

    first

---
+++ src/main/java/com/example/nestdigitalapp2_backend/Nestdigitalapp2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nestdigitalapp2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nestdigitalapp2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nestdigitalapp2BackendApplication.class, args);
+	}
+
+}
